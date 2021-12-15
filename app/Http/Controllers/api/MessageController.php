60118214bfc67519233734b74cc33fa5e1f3e9ad
ajinaufal <ajinaufal69@gmail.com>
2021-12-16 01:45:30 +0700

    feat(List Chat): Make fiture list chat

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -35,7 +35,7 @@ class MessageController extends Controller
             ], 400);
         } else {
             $seen = null;
-            $from = $request->from;
+            $from = auth()->user()->id;
             $to = $request->to;
             $message = $request->message;
             if ($request->reply) {
@@ -44,9 +44,9 @@ class MessageController extends Controller
                 $reply = null;
             }
 
-            $conversations = conversations::where([['one', $request->from], ['two', $request->to]])->orwhere([['one', $request->to], ['two', $request->from]])->get();
-            $conv_side_one = conversations::where('one', $request->from)->get();
-            $conv_side_two = conversations::where('two', $request->from)->get();
+            $conversations = conversations::where([['one', $from], ['two', $to]])->orwhere([['one', $to], ['two', $from]])->get();
+            $conv_side_one = conversations::where('one', $from)->get();
+            $conv_side_two = conversations::where('two', $from)->get();
 
             if ($conversations->count() == 0) {
                 $create_conversation = conversations::create([
@@ -54,7 +54,7 @@ class MessageController extends Controller
                     'two' => $to,
                 ]);
                 $seen = 'one';
-            } else{
+            } else {
                 $create_conversation = $conversations->first();
                 if ($conv_side_one->count() != 0) {
                     $seen = 'one';
@@ -82,21 +82,116 @@ class MessageController extends Controller
                 'message' => 'successful',
                 'data' => array(
                     'From'    => $create_message->get_user->name,
-                    'To'      => $request->to,
+                    'To'      => $to,
                     'Message' => $create_message->message,
                     'reply'   => $request->reply,
                     'Time'    => $create_message->created_at,
-                )], 200);
+                )
+            ], 200);
         }
     }
 
     public function ConvListTwo(Request $request)
     {
-        # code...
+        $validator = Validator::make($request->all(), [
+            'to' => 'required',
+            'message' => 'required',
+        ]);
+
+        $errors = $validator->errors();
+        $message = [
+            'to' => $errors->first('to'),
+            'message' => $errors->first('message'),
+        ];
+        $form_error = [
+            'to' => $errors->has('to'),
+            'message' => $errors->has('message'),
+        ];
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message'           => "error validation",
+                'check_validation'  => $message,
+                'form'              => $form_error,
+            ], 400);
+        } else {
+            $from = auth()->user()->id;
+            $to = $request->to;
+
+            $conversations = conversations::where([['one', $from], ['two', $to]])->orwhere([['one', $to], ['two', $from]])->get();
+            $conv_side_one = conversations::where('one', $from)->get();
+            $conv_side_two = conversations::where('two', $from)->get();
+
+            if ($conv_side_one->count() != 0) {
+                foreach ($conv_side_one->first()->get_message as $key => $value) {
+                    if ($value->seen == 'one') {
+                        messages::find($value->id)->update([
+                            'seen' => 'both',
+                        ]);
+                    }
+                }
+            } elseif ($conv_side_two->count() != 0) {
+                foreach ($conv_side_two->first()->get_message as $key => $value) {
+                    if ($value->seen == 'two') {
+                        messages::find($value->id)->update([
+                            'seen' => 'both',
+                        ]);
+                    }
+                }
+            }
+
+            if ($conversations->count() != 0) {
+                foreach ($conversations->first()->get_message->orderBy('created_at', 'DESC')->get() as $key => $message) {
+                    $chat[$key] = [
+                        'user' => $message->from_id,
+                        'message' => $message->message,
+                        'time' => $message->created_at,
+                    ];
+                }
+            }
+        }
+        if ($conversations->count == 0) {
+            return response()->json([
+                'message'       => 'error',
+                'check_data'    => $conversations,
+            ], 500);
+        }
+        return response()->json([
+            'message' => 'successful',
+            'data' => $chat,
+        ], 200);
     }
 
     public function ConvListAll(Request $request)
     {
-        # code...
+        $validator = Validator::make($request->all(), [
+            'to' => 'required',
+            'message' => 'required',
+        ]);
+
+        $errors = $validator->errors();
+        $message = [
+            'to' => $errors->first('to'),
+            'message' => $errors->first('message'),
+        ];
+        $form_error = [
+            'to' => $errors->has('to'),
+            'message' => $errors->has('message'),
+        ];
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'message'           => "error validation",
+                'check_validation'  => $message,
+                'form'              => $form_error,
+            ], 400);
+        } else {
+            $from = auth()->user()->id;
+            $conversations = conversations::where('one', $from)->orwhere('two', $from)->get();
+            return response()->json([
+                'message' => 'successful',
+                'data' => $conversations,
+            ], 200);
+        }
     }
 }
